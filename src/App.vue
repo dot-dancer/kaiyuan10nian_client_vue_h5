329844b66620915148f54171dcf115c3d2642c2c
dotdancer <dotdancer@163.com>
2022-08-23 23:43:50 +0800

    feature: scss变量-函数-mixin实现一键换肤

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,26 @@
       {{ lpk('Index') }}
       <Icon icon="icon-time"/>
       <van-button type="primary">主要按钮</van-button>
-      <van-calendar v-model:show="show"/>
+      <!-- <van-calendar v-model:show="show"/> -->
+      <div class="theme">
+        <div class="theme-item" v-for="index in 3">{{ index }}</div>
+      </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.theme{
+  display: flex;
+  justify-content: space-between;
+
+  .theme-item{
+    margin: 10px 20px;
+    width: 30%;
+    padding: 30px 0;
+    text-align: center;
+    @include fontSize('big-size');
+    @include bg('menu-bg');
+    @include color('menu-text-color');
+  }
+}
 </style>
